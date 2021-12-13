31ad6eb029f7329f2ad656b986b9876c3b161b05
moaaze151 <moaaze151ds@gmail.com>
2021-12-13 18:07:22 +0200

    complete first template

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let head = document.querySelector("header");
 let iconMenu = document.querySelector(".menu i");
 let sidebar = document.querySelector(".sidebar");
 let iconX = document.querySelector(".sidebar p");
@@ -13,5 +14,14 @@ iconX.addEventListener("click", () => {
     sidebar.style.right = "-240px";
   } else {
     sidebar.style.right = "-330px";
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (scrollY > innerHeight && head.classList[0] !== "head-generic") {
+    head.style.cssText =
+      "position:fixed;top:0;background:#333;padding-top:20px;padding-bottom:20px; left:0;right:0;";
+  } else if(scrollY < innerHeight && head.classList[0] !== "head-generic"){
+    head.style.cssText = "position:absolute;top:25px;background:transparent;";
   }
 });
